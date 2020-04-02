5896e1624072d8c3de91ebf4f731a807bfbc1ae6
Kleber de Souza <klsdesigner@hotmail.com>
2020-04-02 19:26:57 -0300

    modifição na category, comoser.lock, userTableSeeder e criação da StoreTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class, 20)->create();
+        factory(\App\User::class, 20)->create()->each(function($user){
+            $user->store()->save(factory(\App\Store::class)->make());
+        });
     }
 }
